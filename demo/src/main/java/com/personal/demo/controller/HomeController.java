c7334dd255010fd0709c632a2413678aca225e1e
CodeRza17 <muhammedrza.c.m.v@gmail.com>
2026-01-31 17:35:05 +0400

    Layout

---
+++ demo/src/main/java/com/personal/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.personal.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
+    }
+}
---
+++ demo/src/main/java/com/personal/demo/service/demoservie.java
@@ -0,0 +1,4 @@
+package com.personal.demo.service;
+
+public interface demoservie {
+}
